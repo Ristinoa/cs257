fa07afb98cb378e5d633b3757e7cf5fb8d253695
Carl Tankersley <tankersleyc@carleton.edu>
2021-09-27 10:57:24 -0500

    Added first few tests

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -9,6 +9,7 @@ import unittest
 class BooksDataSourceTester(unittest.TestCase):
     def setUp(self):
         self.data_source = booksdatasource.BooksDataSource('books1.csv')
+        self.small_data_source = booksdatasource.BooksDataSource('books2.csv')
 
     def tearDown(self):
         pass
@@ -17,6 +18,27 @@ class BooksDataSourceTester(unittest.TestCase):
         authors = self.data_source.authors('Pratchett')
         self.assertTrue(len(authors) == 1)
         self.assertTrue(authors[0] == Author('Pratchett', 'Terry'))
+
+    def test_unique_title(self):
+        titles = self.data_source.books('Beloved')
+        self.assertTrue(len(titles) == 1)
+        self.assertTrue(titles[0] == Book('Beloved'))
+
+    def test_title_sort(self):
+        titles = self.small_data_source.books()
+        prev_book = titles[0]
+        for book in titles:
+            self.assertLessEqual(prev_book.title.lower(), book.title.lower())
+            prev_book = book.lower()
+
+    def test_default_sort(self):
+        titles = self.small_data_source.books(None, "random_thing")
+        prev_book = titles[0]
+        for book in titles:
+            self.assertLessEqual(prev_book.title.lower(), book.title.lower())
+            prev_book = book.lower()
+
+
 
 if __name__ == '__main__':
     unittest.main()
