7e9729043fc5f3f474124ed63eceb39651c726b8
A.J. Ristino <laurieizutsu@AJs-MacBook-Pro.local>
2021-09-27 16:07:31 -0500

    Made minor changes to books2.csv, wrote tests for year and between_Year sorts.

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -31,6 +31,13 @@ class BooksDataSourceTester(unittest.TestCase):
             self.assertLessEqual(prev_book.title.lower(), book.title.lower())
             prev_book = book.lower()
 
+    def test_author_sort(self):
+        authors = self.small_data_sources.books()
+        prev_book = authors[0]
+        for name in authors:
+             self.asserLessEqual(prev_book.author.lower(),book.author.lower())
+             prev_book = book
+
     def test_default_sort(self):
         titles = self.small_data_source.books(None, "random_thing")
         prev_book = titles[0]
@@ -38,6 +45,28 @@ class BooksDataSourceTester(unittest.TestCase):
             self.assertLessEqual(prev_book.title.lower(), book.title.lower())
             prev_book = book.lower()
 
+    def test_between_year_sort(self):
+        years = self.small_data_sources.books(1945, 2010)
+        prev_book = years[0]
+        for book in years:
+            self.assertLessEqual(book.year(),2010)
+            self.assertGreaterEqual(book.year(),1945)
+            self.assertLessEqual(prev_book.year(),book.year())
+            if(prev_book.year() == book.year()):
+                self.assertLessEqual(prev_book.title.lower(), book.title.lower())
+                prev_book = book.lower()
+            else:
+                prev_book = book.lower()
+
+    def test_between_year_sort(self):
+        years = self.small_data_sources.books()
+        prev_book = years[0]
+        for book in years:
+            if(prev_book.year() == book.year()):
+                self.assertLessEqual(prev_book.title.lower(), book.title.lower())
+                prev_book = book.lower()
+            else:
+                prev_book = book.lower()
 
 
 if __name__ == '__main__':
