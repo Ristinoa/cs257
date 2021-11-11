9540a27ad45e34955d1611d17f5c9fd7e1f12545
A.J. Ristino <laurieizutsu@AJs-MacBook-Pro.local>
2021-11-10 22:37:25 -0600

    Renamed 'final' to 'webapp' as well as created the folders and files for the end-to-end system setup

---
+++ webapp/api.py
@@ -0,0 +1,7 @@
+'''api.py'''
+
+'''Authors: A.J. Ristino & Julian Bowers'''
+
+'''A Flask blueprint to contain routes that serve API endpoints'''
+
+
---
+++ webapp/app.py
@@ -0,0 +1,5 @@
+'''app.py'''
+
+'''Authors: A.J. Ristino & Julian Bowers'''
+
+'''A Flask app containing routes that serve HTML pages'''
